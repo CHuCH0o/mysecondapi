bf1f0ad8c06cc44203342e5aeba56a0caf49afa4
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 11:41:52 -0500

    Actualizacion clase abstracta Parameter

--- src/main/java/co/edu/umanizales/mysecondapi/model/Parameter.java
+++ src/main/java/co/edu/umanizales/mysecondapi/model/Parameter.java
@@ -2,10 +2,12 @@ package co.edu.umanizales.mysecondapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-public class Parameter {
-    private String code;
-    private String description;
+@NoArgsConstructor
+public abstract class Parameter {
+    private String code;         // Código del parámetro
+    private String description;  // Descripción del parámetro
 }
