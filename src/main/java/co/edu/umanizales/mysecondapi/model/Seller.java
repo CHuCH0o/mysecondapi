bdfb2af72418b39b70064f8720bc04be2f6b8596
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 11:24:37 -0500

    Actualizacion clase Seller

--- src/main/java/co/edu/umanizales/mysecondapi/model/Seller.java
+++ src/main/java/co/edu/umanizales/mysecondapi/model/Seller.java
@@ -2,10 +2,16 @@ package co.edu.umanizales.mysecondapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Seller {
-    private String id;
+    private String identification;
+    private Parameter typeDoc; // Será una instancia de TypeDoc
     private String name;
+    private String lastName;
+    private byte age;
+    private Location city;
 }
