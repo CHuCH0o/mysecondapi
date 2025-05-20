1e89c82790569f49211532acca0260a60d976877
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 11:54:22 -0500

    creacion clase heredada de Parameter / TypeProduct

---
+++ src/main/java/co/edu/umanizales/mysecondapi/model/TypeProduct.java
@@ -0,0 +1,12 @@
+package co.edu.umanizales.mysecondapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeProduct extends Parameter {
+    // Hereda code y description de Parameter
+}
