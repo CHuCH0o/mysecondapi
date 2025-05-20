c8a23d58393fbffc23632fb8722a812319a13c8d
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 11:31:22 -0500

    Actualizacion clase Product

--- src/main/java/co/edu/umanizales/mysecondapi/model/Product.java
+++ src/main/java/co/edu/umanizales/mysecondapi/model/Product.java
@@ -2,10 +2,14 @@ package co.edu.umanizales.mysecondapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
-    private double price;
-    private int stock;
+    private String code;                // Código único del producto
+    private double price;              // Precio del producto
+    private int stock;                 // Cantidad en inventario
+    private Parameter typeProduct;     // Tipo de producto (instancia de TypeProduct)
 }
