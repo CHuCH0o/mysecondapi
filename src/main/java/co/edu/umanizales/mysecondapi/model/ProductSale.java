fa7a3b51fe0a9aa42eee3575c7ea29db6e2805f4
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 11:39:01 -0500

    Actualizacion DTO ProductSale

--- src/main/java/co/edu/umanizales/mysecondapi/model/ProductSale.java
+++ src/main/java/co/edu/umanizales/mysecondapi/model/ProductSale.java
@@ -1,4 +1,14 @@
 package co.edu.umanizales.mysecondapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductSale {
+    private String code;       // Código del producto vendido
+    private int quantity;      // Cantidad de unidades vendidas
+    private double subtotal;   // Precio total por este producto (price × quantity)
 }
