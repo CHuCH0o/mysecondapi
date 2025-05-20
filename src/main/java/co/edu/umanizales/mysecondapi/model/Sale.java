9ca41baf8d37b9c133e8e8bf625e9a883e177a82
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 11:35:02 -0500

    Actualizacion clase Sale

--- src/main/java/co/edu/umanizales/mysecondapi/model/Sale.java
+++ src/main/java/co/edu/umanizales/mysecondapi/model/Sale.java
@@ -2,16 +2,19 @@ package co.edu.umanizales.mysecondapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
-import java.util.Locale;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Sale {
-    private Store store;
-    private Seller seller;
-    private Locale dateSale;
-    private int quantity ;
-    private String products;
+    private Store store;                        // Tienda donde se realizó la venta
+    private Seller seller;                      // Vendedor que hizo la venta
+    private LocalDate dateSale;                 // Fecha de la venta
+    private int quantity;                       // Cantidad total de productos vendidos
+    private List<ProductSale> products;         // Lista de productos vendidos, conexion con DTO
+    private double totalSale;                   // Monto total de la venta
 }
