a8555b53f40265f984d886b8935b945a2ad9b717
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 11:48:05 -0500

    Creacion clase heredada de Parameter / TypeDocument

---
+++ src/main/java/co/edu/umanizales/mysecondapi/model/TypeDocument.java
@@ -0,0 +1,12 @@
+package co.edu.umanizales.mysecondapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeDocument extends Parameter {
+    // Hereda code y description de Parameter
+}
