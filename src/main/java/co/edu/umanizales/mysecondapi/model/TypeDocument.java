c8c83aab51876f116f433374d3da798c66d56b98
CHuCH0o <jgutierrez113077@umanizales.edu.co>
2025-05-20 21:58:12 -0500

    atualizacion y correccion de errores

--- src/main/java/co/edu/umanizales/mysecondapi/model/TypeDocument.java
+++ src/main/java/co/edu/umanizales/mysecondapi/model/TypeDocument.java
@@ -1,11 +1,14 @@
 package co.edu.umanizales.mysecondapi.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class TypeDocument extends Parameter {
+
     public TypeDocument(String code, String description) {
         super(code, description);
     }
--- src/main/java/co/edu/umanizales/mysecondapi/model/TypeProduct.java
+++ src/main/java/co/edu/umanizales/mysecondapi/model/TypeProduct.java
@@ -1,9 +1,11 @@
 package co.edu.umanizales.mysecondapi.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class TypeProduct extends Parameter {
 
